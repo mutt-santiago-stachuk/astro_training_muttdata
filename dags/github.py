7a6c976addaf0ee8ca842d0f3081664fac89cfad
zelda-mutt <santiago.stachuk@muttdata.ai>
2024-06-24 16:42:06 -0300

    Create github.py
    change name to github

---
+++ dags/github.py
@@ -0,0 +1,16 @@
+from airflow.decorators import dag, task
+from datetime import datetime
+
+@dag(
+    schedule=None,
+    catchup=False,
+    default_args={'retries': 2},
+    tags=["example"]
+)
+def github():
+    @task
+    def my_task():
+        return "include/sql/SELECT_from_my_table.sql"
+    my_task()
+
+github()
